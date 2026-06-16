f419cd974f0991870107f17aab753f3182049bee
abedh <abed.haj@live.com>
2026-06-16 19:42:53 +0300

    Fixed database connection issues and ENV dependencies

--- server.js
+++ server.js
@@ -15,25 +15,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-
-// Comment out the env version for a moment
-// mongoose.connect(process.env.MONGODB_URI)
-
-// Hardcode the new string directly
 console.log("THE ACTUAL STRING NODE IS USING IS:", process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then((conn) => {
       console.log(`Successfully connected to MongoDB`);
-      console.log(`Database Host: ${conn.connection.host}`); // <--- Add this line
   })
   .catch((err) => console.error('MongoDB connection error:', err));
-//const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/social_network';
-//mongoose.connect(process.env.MONGODB_URI).then(() => console.log('MongoDB connected')).catch((err) => console.error('MongoDB connection error:', err));
-// Connect to MongoDB (Updated to the modern syntax without deprecated options)
-/*mongoose.connect(MONGODB_URI)
-.then(() => console.log('Successfully connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));*/
+
 
 // Connect the routes to the app
 app.use('/api/users', userRoutes);
