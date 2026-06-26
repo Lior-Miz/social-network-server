0c0ebb7930ef4732ed62d37b524cd318dd05b293
abedh <abed.haj@live.com>
2026-06-26 14:07:37 +0300

    fixed a problem with auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+const auth = require('../middlewares/auth');
 
 router.post('/register', userController.registerUser); //register
 router.post('/login', userController.loginUser); //login
