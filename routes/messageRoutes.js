47d8958d9e02f269771113e25eae6b6e1eaac73b
abedh <abed.haj@live.com>
2026-07-02 13:58:19 +0300

    deleted everything related to messages

--- routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-/*const express = require('express');
-const router = express.Router();
-const messageController = require('../controllers/messageController');
-const auth = require('../middlewares/auth'); // Assuming you have auth middleware
-
-router.post('/', auth, messageController.sendMessage);
-router.get('/:groupId', auth, messageController.getMessages);
-
-module.exports = router;*/
